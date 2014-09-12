67b1cf27ff9bec1ca91fd4d35eb62eb17c7cc591
bradstrong <bradcstrong@gmail.com>
2014-09-12 17:17:07 -0400

    add mailchimp settings

--- client/views/events/event_details.js
+++ client/views/events/event_details.js
@@ -1,4 +1,11 @@
 Template.eventDetails.helpers({
+    eventScenes: function(){
+        var getScenes = Scenes.find( { _id: { $in: this.scene } } ).fetch(); //find all scenes that are present in the current document's scene array
+        return getScenes;
+    },
+    eventAttendees: function(){
+        return 'poop';
+    }
   // guyView: function() { // If the user is currently logged in and has not already RSVP-ed, allow them to attend
   //   var UserId = Meteor.userId();
   //   var EventId = this._id;
