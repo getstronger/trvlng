3c44dfce5762ef637a451f41fdfc892ab424c9b3
bradstrong <bradcstrong@gmail.com>
2014-09-15 14:38:16 -0400

    add event information to event page

---
+++ collections/conversations.js
@@ -0,0 +1,11 @@
+Conversations = new Meteor.Collection('conversations');
+//Scenes.attachSchema(Schemas.scene);
+
+//_id:
+//createdAt:
+//name:
+//members:
+//owner:
+//picture:
+//lastUpdated:
+
--- collections/messages.js
+++ collections/messages.js
@@ -1,1 +1,10 @@
 Messages = new Meteor.Collection('messages');
+
+//_id:
+//createdAt:
+//author: ''
+//conversation: ''
+//sent:
+//seenBy: []
+//location:
+//attachments: []
