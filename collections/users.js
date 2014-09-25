a8e153646a6fb35e7f0ac5dd37f5816655aac894
bradstrong <bradcstrong@gmail.com>
2014-09-25 17:35:19 -0400

    attach user schema

--- collections/users.js
+++ collections/users.js
@@ -1,4 +1,28 @@
-// Meteor.users.attachSchema(Schema.User);
+//# _id
+//# username
+//# slug
+//# pi - profile image
+//# activeChannel
+//# activeChannelWhen
+//# chatColor
+//# createdAt
+//# deviceUUIDs []
+//# mobilePushId
+//# lastLocation
+//# # type
+//# # coordinates
+//# # accuracy
+//# lastLocationWhen
+//# notificationCount
+//# pushChannels [] - the channelIds this user receives push notifications for
+//    # localSystemChannels [] - currently joined local system channel ids
+//# localChannels [] - the currently joined local channel ids
+//# globalChannels [] - the current joined global channel ids
+//# openPrivateChannels [] ** TODO: change this to privateChannels to match convention
+//# services {}
+
+
+Meteor.users.attachSchema(Schemas.User);
 
 // Meteor.users.allow({
 //   update: function(userId, doc){
@@ -8,3 +32,12 @@
 //     return isAdminById(userId) || userId == doc._id;
 //   }
 // });
+
+//{
+//    Ryan: 1380422897
+//    Mike: 1066517454
+//    Anthony: 9391131
+//    jon:
+//chris: 507746643
+//brad fb:  http://res.cloudinary.com/trvlng/image/facebook/8214216.jpg
+//}
