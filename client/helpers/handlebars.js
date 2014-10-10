8ab7f39b893891ac071c22f4d679bd0fb73c815f
bradstrong <bradcstrong@gmail.com>
2014-10-10 17:08:51 -0400

    add domain and googleMapsUrl helpers

--- client/helpers/handlebars.js
+++ client/helpers/handlebars.js
@@ -21,3 +21,40 @@ UI.registerHelper('activeRouteClass', function(/* route names */) {
 UI.registerHelper("formatDate", function(datetime, format) {
   return moment(datetime).format(format);
 });
+
+UI.registerHelper("domain", function(url) { // Display the domain of the event's external website
+  var a = document.createElement('a');
+  a.href = url;
+  return a.hostname;
+});
+
+UI.registerHelper("googleMapsUrl", function(location){
+  var gmQueryBaseUrl = "http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=";
+
+  var queryBaseUrl = gmQueryBaseUrl;
+  var originalString = location;
+  var obj = location;
+/* */
+  var querifyString = function(obj) {
+    var str = '';
+    for (var p in obj) {
+      if (obj.hasOwnProperty(p)) {
+        str += obj[p] + ' ';
+      }
+    }
+    str = str.slice(0,-1); //trim final '+' from string
+    return str;
+  }
+
+  var locationString = querifyString(originalString);
+  var regex = /(\s+)/igm;
+  var newString = locationString.replace(regex, '+');
+  return queryBaseUrl+newString;
+
+});
+
+//http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=undefined9+Columbus+SquaregayborhoodPhiladelphiaPA
+
+/*
+ $('#test').html('poop');*/
+//updateString(gmQueryBaseUrl, eventLocation);
