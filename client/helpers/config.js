c2be99d4ca78e44e41a2426f1cdab1c06bf7195d
bradstrong <bradcstrong@gmail.com>
2014-11-03 17:26:57 -0500

    update to meteor 1.0
    update router to comply with iron router 1.0 syntax

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -3,17 +3,17 @@ Accounts.ui.config({
 });
 
 Meteor.startup(function () {
-    AccountsEntry.config({
-      logo: 'img/trvlng.svg',                  // if set displays logo above sign-in options
-      privacyUrl: '/privacy-policy',     // if set adds link to privacy policy and 'you agree to ...' on sign-up page
-      termsUrl: '/terms-of-use',         // if set adds link to terms  'you agree to ...' on sign-up page
-      homeRoute: '/',                    // mandatory - path to redirect to after sign-out
-      dashboardRoute: '/events/new',      // mandatory - path to redirect to after successful sign-in
-      profileRoute: '/profile/' + Meteor.userId(),
-      passwordSignupFields: 'USERNAME_AND_EMAIL',
-      showSignupCode: false,
-      showOtherLoginServices: true      // Set to false to hide oauth login buttons on the signin/signup pages. Useful if you are using something like accounts-meld or want to oauth for api access
-    });
+    //AccountsEntry.config({
+    //  logo: 'img/trvlng.svg',                  // if set displays logo above sign-in options
+    //  privacyUrl: '/privacy-policy',     // if set adds link to privacy policy and 'you agree to ...' on sign-up page
+    //  termsUrl: '/terms-of-use',         // if set adds link to terms  'you agree to ...' on sign-up page
+    //  homeRoute: '/',                    // mandatory - path to redirect to after sign-out
+    //  dashboardRoute: '/events/new',      // mandatory - path to redirect to after successful sign-in
+    //  profileRoute: '/profile/' + Meteor.userId(),
+    //  passwordSignupFields: 'USERNAME_AND_EMAIL',
+    //  showSignupCode: false,
+    //  showOtherLoginServices: true      // Set to false to hide oauth login buttons on the signin/signup pages. Useful if you are using something like accounts-meld or want to oauth for api access
+    //});
   });
 
 SharrreOptions={
